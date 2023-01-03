c98e0ff75b77464bf26edc79a5c5ac8660de6a61
Marcelo <marcelosimim@gmail.com>
2023-01-03 09:25:54 -0300

    tap gesture

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Marcelo Simim Santos on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Marcelo Simim Santos on 1/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    // MARK: - FUNCTION
+
+    // MARK: - CONTENT
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1 TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            } //: ZTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
