8f03e650079fff096ad9d9f885abc58dd7a1f563
Marcelo <marcelosimim@gmail.com>
2023-01-03 09:50:24 -0300

    drag gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,8 +11,14 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     // MARK: - FUNCTION
-
+    func resetImageScale() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     // MARK: - CONTENT
     var body: some View {
         NavigationView {
@@ -25,19 +31,36 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
-                // MARK: - 1 TAP GESTURE
-                    .onTapGesture(count: 2) {
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
                             withAnimation(.spring()) {
-                                imageScale = 1
+                                resetImageScale()
                             }
                         }
-                    }
+                    })
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    withAnimation(.spring()) {
+                                        resetImageScale()
+                                    }
+                                }
+                            })
+                    )
             } //: ZTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
